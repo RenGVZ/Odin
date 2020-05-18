3ec94fa5fa57261b7ae47b35a0b1c2c3d2930d4f
gavingarza <gaving@live.com>
2020-05-18 23:32:33 +0900

    added a random canvas circle generator

--- script-practice/favNumber.js
+++ script-practice/favNumber.js
@@ -3,13 +3,15 @@ const button = document.querySelector("button");
   let answer = document.getElementById("answer");
   
   // console.log(response);
-  button.onclick = function() {
-    let number = prompt('what is your favorite number');
-    if (number == 42) {
-      answer.innerText = "Yay! That's my favorite too!"
-    } else if(number < 42) {
-      answer.innerText = `Eh, that's OK but ${parseInt(number) + 1} would be even better`
-    } else {
-      answer.innerText = "LAME. That number is too large!"
+  button.addEventListener('click', numFunc)
+    
+  function numFunc() {
+      let number = prompt('what is your favorite number');
+      if (number == 42) {
+        answer.innerText = "Yay! That's my favorite too!"
+      } else if(number < 42) {
+        answer.innerText = `Eh, that's OK but ${parseInt(number) + 1} would be even better`
+      } else {
+        answer.innerText = "LAME. That number is too large!"
+      }
     }
-  }
