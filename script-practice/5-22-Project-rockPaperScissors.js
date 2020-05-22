1066896bd3b762bf9634a217010709651884eccf
gavingarza <gaving@live.com>
2020-05-22 23:11:27 +0900

    created a rock-paper-scissors console app

---
+++ script-practice/5-22-Project-rockPaperScissors.js
@@ -0,0 +1,35 @@
+function playRound(playerSelection, computerSelection) {
+  if(playerSelection === computerSelection){
+    return "tie";
+  } else if (playerSelection == "rock" && computerSelection == "scissors"){
+    return "player wins";
+  } else if (playerSelection == "rock" && computerSelection == "paper"){
+    return "computer wins";
+  } else if (playerSelection == "scissors" && computerSelection == "rock"){
+    return "computer wins";
+  } else if (playerSelection == "scissors" && computerSelection == "paper"){
+    return "player wins";
+  } else if (playerSelection == "paper" && computerSelection == "rock"){
+    return "player wins";
+  } else if (playerSelection == "paper" && computerSelection == "scissors"){
+    return "computer wins";
+  }
+};
+
+const computerSelection = computerPlay();
+const playerSelection = playerPlay();
+
+function playerPlay() {
+  const playerChoice = prompt("rock, paper, or scissors?");
+  return playerChoice;
+}
+
+function computerPlay() {
+  const array = ["rock", "paper", "scissors"];
+  const choice = array[Math.floor(Math.random() * array.length)];
+  // const selection = array[choice];
+  console.log("Cheater's results- computer: " + choice);
+  return choice;
+}
+
+console.log(playRound(playerSelection, computerSelection));
