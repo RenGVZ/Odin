2b5c22b7e215202163580a60e437a6ff024d2f29
gavingarza <gaving@live.com>
2020-05-26 21:53:45 +0900

    Completed a variety of javaScripts challenges

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (num1 < 0 || num2 < 0) return 'ERROR';
+  if (typeof num1 !== "number" || typeof num2 !== "number") return 'ERROR';
 
+  if (num1 > num2) {
+    const temp = num1;
+    num1 = num2;
+    num2 = temp;
+  }
+
+  let sum = 0
+  for (let x = num1; x < num2 + 1; x++) {
+    sum += x;
+  }
+  return sum;
 }
+
+console.log(sumAll(1, 5));
 
 module.exports = sumAll
--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celsius = (temp - 32) * (5/9);
+  celsiusDecimal = Math.round(celsius * 10) / 10;
+  return celsiusDecimal;
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let fahrenheit = ((temp) * (9/5)) + 32;
+  fahrenheitDecimal = Math.round(fahrenheit * 10) / 10;
+  return fahrenheitDecimal;
 }
 
 module.exports = {
