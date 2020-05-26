6744e8753b82d29020a2ef0d13591cf6dafbb0a1
gavingarza <gaving@live.com>
2020-05-26 18:06:10 +0900

    Odin Test Exercises

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+  if (word === '') {
+    return '';
+  } else if (num < 0) {
+    return 'ERROR';
+  } else {
+    let string = '';
+    for (let x = 0; x < num; x++) {
+      string += word;
+    }
+  return string;
+}
 }
 
 module.exports = repeatString
