ba47d43a53e8e7eaa249aeed1e027756e14bcb8c
gavingarza <gaving@live.com>
2020-05-27 23:43:07 +0900

    finished all gameplay functionality for r-p-s app

--- odin_projects/updated-rock-paper-scissors/script.js
+++ odin_projects/updated-rock-paper-scissors/script.js
@@ -2,18 +2,41 @@
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
-const left = document.querySelector('#left');
-const right = document.querySelector('#right');
+const left = document.querySelector('#left > p');
+const right = document.querySelector('#right > p');
 const para = document.querySelector('#paragraph');
+const displayPlayer = document.querySelector('#display-player');
+const displayComputer = document.querySelector('#display-computer');
+const displayRound = document.querySelector('#display-round');
+
+let playerScore = 0;
+let computerScore = 0;
+let roundCount = 0;
 
 // GamePlay Functions
 function playRound(playerSelection, computerSelection) {
   if(playerSelection === computerSelection){
+    playerScore++;
+    computerScore++;
+    roundCount++;
+    displayRound.textContent = roundCount;
+    displayPlayer.textContent = playerScore;
+    displayComputer.textContent = computerScore;
     return "It's a tie 🏳️";
   } else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "rock")){
+    playerScore++;
+    roundCount++;
+    displayRound.textContent = roundCount;
+    displayPlayer.textContent = playerScore;
+    displayComputer.textContent = computerScore;
     return "You win! 😀";
   } else if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "scissors" && computerSelection == "rock") || (playerSelection == "paper" && computerSelection == "scissors")){
-    return "You lose 😞";
+    computerScore++;
+    roundCount++;
+    displayRound.textContent = roundCount;
+    displayPlayer.textContent = playerScore;
+    displayComputer.textContent = computerScore;
+    return "You lose 💩";
   };
 };
 function computerPlay() {
@@ -38,3 +61,17 @@ rock.addEventListener('click', rockPlay);
     left.textContent = "🗿Rock!"
     para.textContent = playRound(playerSelection, computerSelection);
   };
+paper.addEventListener('click', paperPlay);
+  function paperPlay () {
+    const playerSelection = 'paper';
+    const computerSelection = computerPlay();
+    left.textContent = "📄 Paper";
+    para.textContent = playRound(playerSelection, computerSelection);
+  }
+scissors.addEventListener('click', scissorsPlay);
+  function scissorsPlay () {
+    const playerSelection = 'scissors';
+    const computerSelection = computerPlay();
+    left.textContent = "💇‍♂️ Scissors";
+    para.textContent = playRound(playerSelection, computerSelection);
+  }
