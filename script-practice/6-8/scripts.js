14d86b73873d1d77eb3f5779869262653e458899
gavingarza <gaving@live.com>
2020-06-10 23:46:12 +0900

    added skip buttons and hooked them up to skip according to innervalue on event listener clicks

---
+++ script-practice/6-8/scripts.js
@@ -0,0 +1,38 @@
+// Get our elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+// Build our function
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? '▶︎' : '◼︎◼︎';
+  console.log(icon);
+  toggle.textContent = icon;
+}
+
+function skip() {
+ console.log(this.dataset.skip);
+ video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  console.log(this.value);
+}
+
+// Hook up our event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+console.log(skipButtons);
