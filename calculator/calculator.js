3e04685ba97438782f34d683bd4c44911e88868d
gavingarza <gaving@live.com>
2020-06-17 23:05:32 +0900

    Working on programming problems related to mathematical functions

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,22 +1,42 @@
-function add () {
-	
+function add (x, y) {
+	return x + y;
 }
 
-function subtract () {
-	
+function subtract (x, y) {
+	return x - y;
 }
 
-function sum () {
-	
+
+function sum (array) {
+	let total = 0;
+
+	if (array.length == 0) {
+		return 0;
+	} 
+	else if (array.length >= 2) {
+		array.map(num => {
+			total += num;
+		})
+		return total;
+	} else {
+		return array[0];
+	}
+}
+// console.log(sum([5, 5, 5, 6, 365]));
+
+function multiply (array) {
+	return array.length 
+		? array.reduce((accumulator, nextItem) => accumulator * nextItem) : 0;
+	};
+
+// const a = [2,4,6];
+// console.log(multiply(a));
+
+function power(int, exp) {
+	return Math.pow(int, exp);
 }
 
-function multiply () {
-	
-}
-
-function power() {
-	
-}
+console.log(power(4, 3));
 
 function factorial() {
 	
