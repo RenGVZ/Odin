054929ba5c45eface40d63888cb300e28f0fb2b9
gavingarza <gaving@live.com>
2020-06-21 18:11:28 +0900

    applied finishing touches to mini app

--- script-practice/6-20/form_project/script.js
+++ script-practice/6-20/form_project/script.js
@@ -2,6 +2,8 @@ const submitBtn = document.getElementById('submit-btn');
 const color1 = document.getElementById('color1');
 const color2 = document.getElementById('color2');
 const form = document.querySelector('form');
+const label1 = document.querySelector('#label1');
+const label2 = document.querySelector('#label2');
 
 const colorCircle1 = document.getElementById('color-circle1');
 const colorCircle2 = document.getElementById('color-circle2');
@@ -11,6 +13,8 @@ submitBtn.addEventListener('click', (e) => {
   document.body.style.backgroundImage = `linear-gradient(to left, ${color2.value}, ${color1.value})`;
   form.style.background = `${color2.value}`;
   submitBtn.style.background = `${color1.value}`;
+  label1.style.background = `${color1.value}`;
+  label2.style.background = `${color1.value}`;
 });
 
 color1.addEventListener('change', (e) => {
