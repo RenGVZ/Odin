7d86da2fba9aede5edd6179315b34a5772a91b1c
gavingarza <gaving@live.com>
2020-06-21 16:26:10 +0900

    created a webapp that changes the page color upon selection

---
+++ script-practice/6-20/form_project/script.js
@@ -0,0 +1,24 @@
+const submitBtn = document.getElementById('submit-btn');
+const color1 = document.getElementById('color1');
+const color2 = document.getElementById('color2');
+const form = document.querySelector('form');
+
+const colorCircle1 = document.getElementById('color-circle1');
+const colorCircle2 = document.getElementById('color-circle2');
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  document.body.style.backgroundImage = `linear-gradient(to left, ${color2.value}, ${color1.value})`;
+  form.style.background = `${color2.value}`;
+  submitBtn.style.background = `${color1.value}`;
+});
+
+color1.addEventListener('change', (e) => {
+  console.log(e);
+  colorCircle1.style.background = `${color1.value}`;
+})
+
+color2.addEventListener('change', (e) => {
+  console.log(e);
+  colorCircle2.style.background = `${color2.value}`;
+})
