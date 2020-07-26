d7396aea51a920b3e7838d917b72e47ca570e904
Gavin Garza <48096041+RenGVZ@users.noreply.github.com>
2020-07-26 15:59:10 +0900

    got the functions to compute when operator is called

--- script-practice/7-25/calc2.js
+++ script-practice/7-25/calc2.js
@@ -1,38 +1,97 @@
-const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operation]');
+let newVal = ''
+let prevVal = ''
+let operation = ''
+let result = ''
+
+const currentDisplay = document.querySelector('[data-current]');
+const prevDisplay = document.querySelector('[data-prev]');
 const equals = document.querySelector('[data-equals]');
-const display = document.querySelector('[data-current]');
+const allClear = document.querySelector('[data-all-clear]');
 
-let firstNum = ''
-let secondNum = ''
-let operator = ''
+function getNumbers(num) {
+  newVal += num;
+  currentDisplay.innerText = newVal;
+}
 
-numberButtons.forEach(button => {
-  button.addEventListener('click', (event) => {
-    firstNum += event.target.innerText;
-    displayNum();
-  });
+function getOperator(operator) {
+  operation = operator;
+  prevVal = newVal;
+  newVal = '';
+}
+
+equals.addEventListener('click', function compute() {
+  switch (operation) {
+    case '+':
+      result = parseInt(prevVal) + parseInt(newVal);
+      break
+    case '-':
+      result = parseInt(prevVal) - parseInt(newVal);
+      break
+    case 'x':
+      result = parseInt(prevVal) * parseInt(newVal);
+      break
+    case '÷':
+      result = parseInt(prevVal) / parseInt(newVal);
+      break
+    case '%':
+      result = parseInt(prevVal) % parseInt(newVal);
+      break
+    default:
+      return
+  }
+  return currentDisplay.innerText = result
 });
 
-function displayNum() {
-  display.innerText = firstNum + secondNum + operator
-}
-
-operationButtons.forEach(operation => {
-  operation.addEventListener('click', (event) => {
-    operator = event.target.innerText;
-    secondNum += event.target.innerText
-    displaySecondNum();
-    // operate(firstNum, operator)
-  })
+allClear.addEventListener('click', () => {
+  newVal = '';
+  prevVal = '';
+  operation = '';
+  result = '';
+  currentDisplay.innerText = '';
 })
 
-function displaySecondNum() {
-  display.innerText = secondNum
-}
 
-equals.addEventListener('click', (operate));
 
-function operate() {
-  console.log(firstNum, operator, secondNum)
-}
+
+
+
+
+
+
+// const operationButtons = document.querySelectorAll('[data-operation]');
+// const equals = document.querySelector('[data-equals]');
+// const display = document.querySelector('[data-current]');
+
+// let firstNum = ''
+// let secondNum = ''
+// let operator = ''
+
+// numberButtons.forEach(button => {
+//   button.addEventListener('click', (event) => {
+//     firstNum += event.target.innerText;
+//     displayNum();
+//   });
+// });
+
+// function displayNum() {
+//   display.innerText = firstNum + secondNum + operator
+// }
+
+// operationButtons.forEach(operation => {
+//   operation.addEventListener('click', (event) => {
+//     operator = event.target.innerText;
+//     secondNum += event.target.innerText
+//     displaySecondNum();
+//     // operate(firstNum, operator)
+//   })
+// })
+
+// function displaySecondNum() {
+//   display.innerText = secondNum
+// }
+
+// equals.addEventListener('click', (operate));
+
+// function operate() {
+//   console.log(firstNum, operator, secondNum)
+// }
